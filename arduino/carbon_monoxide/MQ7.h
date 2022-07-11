e9134001b8965dc0589f1ac48a83fdbe8fa7c6ed
Ganning Xu <ganningxu@gmail.com>
2022-07-11 10:12:49 -0400

    rearrage folder structure

--- arduino/carbon_monoxide/MQ7.h
+++ arduino/carbon_monoxide/MQ7.h
@@ -11,11 +11,11 @@
 #include "Arduino.h"
 
 /*
-	CONFIGURATION OPTIONS 
+	CONFIGURATION OPTIONS
 */
 #define CALIBRATION_SECONDS 15
 //#define _R0 0.025	// uncomment to define hardcoded R0 value
-					// may be desireable if no calibration delay wanted
+// may be desireable if no calibration delay wanted
 
 /*
 IMPORTANT:
@@ -32,7 +32,7 @@ and variable
 	R0	sensor resistance at 100ppm CO in clean air
 NB: ppm == 100 defines Rs/R0 == 1.
 The device reads analog pin, which can be translated into Rs/RL where RL is a resistance load.
-In the datasheet, RL is said to be ~10 kOhm, thus we use 
+In the datasheet, RL is said to be ~10 kOhm, thus we use
 	Rs = (Rs/RL) * RL
 Calculated fitted parameters using inverse of ppm(Rs/R0) and plot in datasheet:
 */
@@ -40,7 +40,7 @@ Calculated fitted parameters using inverse of ppm(Rs/R0) and plot in datasheet:
 #define _COEF_A0 100.0
 #define _COEF_A1 -1.513
 
-/* 
+/*
 datasheet provides typical load resistance RL to be ~10 kOhm
 */
 #define _LOAD_RES 10.0
@@ -51,18 +51,21 @@ need to calibrate device by knowing R0 -- in clean air, approx 10ppm, ratio is
 */
 #define _CALIBRATION_CONSTANT 5.0
 
-class MQ7 {
+class MQ7
+{
 public:
-	MQ7(int pin) : a_pin(pin), v_in(5.0) { 
-		;	// 5.0 Volts assumed / recommended
+	MQ7(int pin) : a_pin(pin), v_in(5.0)
+	{
+		; // 5.0 Volts assumed / recommended
 	}
-	MQ7(int pin, float v_in) : a_pin(pin), v_in(v_in) {
+	MQ7(int pin, float v_in) : a_pin(pin), v_in(v_in)
+	{
 		;
 	}
 
 	float readPpm();
 	float readRs();
-	float readRsRL();	// Rs / RL
+	float readRsRL(); // Rs / RL
 	void calibrate();
 
 	float getR0();
@@ -71,15 +74,15 @@ private:
 	int a_pin;
 	float v_in;
 
-	#ifndef _R0 // If no hardcoded R0 value
+#ifndef _R0 // If no hardcoded R0 value
 
 	float R0;
 
-	#else 		// else make constant
+#else // else make constant
 
 	const R0 = _R0;
 
-	#endif		// end
+#endif // end
 
 	float convertVoltage(int voltage);
 };
